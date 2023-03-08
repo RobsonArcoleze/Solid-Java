f30c5472d4a0ec225d05c2aae0d4339265472655
RobsonArcoleze <robson.arcoleze29@gmail.com>
2023-03-08 16:06:43 -0300

    refactor: Open Closed Principle
    Aplicado o segundo principio de SOLID, para que a minha aplicação seja extensivel. Estava ocorrendo duplicação de código e, impossibilidade de acrescentar regras de negócios. Implementado uma interface, criado uma classe para cada regra de negócio, com isso foi possivel implementar o principio do polimorfismo, bem como a impossibilidade de quebra do código, com futuras adições de regras de validação

---
+++ rh/src/br/com/alura/rh/services/ValidacaoPeriodicidadeEntreReajustes.java
@@ -0,0 +1,21 @@
+package br.com.alura.rh.services;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+import br.com.alura.rh.ValidacaoException;
+import br.com.alura.rh.model.Funcionario;
+
+public class ValidacaoPeriodicidadeEntreReajustes implements ValidacaoReajuste {
+
+	public void validar (Funcionario funcionario, BigDecimal aumento) {
+		
+		LocalDate dataUltimoReajuste = funcionario.getDataUltimoReajuste();
+		LocalDate dataAtual = LocalDate.now();
+		Long mesesDesdeOUltimoReajuste = ChronoUnit.MONTHS.between(dataUltimoReajuste, dataAtual);
+		if (mesesDesdeOUltimoReajuste < 6) {
+			throw new ValidacaoException("Intevalo entre reajustes deve ser de no minimo de 6 mesese");
+		}
+	}
+}
