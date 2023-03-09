b4f0eef2918c26c7f0bf1cd66c75a8d19d24e122
RobsonArcoleze <robson.arcoleze29@gmail.com>
2023-03-08 22:58:23 -0300

    refactor: Liskov Substitution Principle
    Conforme versão anterior, herança estava quebrando as regras de negocios e ferindo o principio de Liskov. Para contornar esse problema, foi gerado uma composição, evitando quebrar o principio de Liskov, e repetição de código

--- rh/src/br/com/alura/rh/model/Terceirizado.java
+++ rh/src/br/com/alura/rh/model/Terceirizado.java
@@ -2,9 +2,15 @@ package br.com.alura.rh.model;
 
 import java.math.BigDecimal;
 
-public class Terceirizado extends Funcionario{
-
+public class Terceirizado {
+	
+	private DadosPessoais dadosPessoais;
 	private String empresa;
+
+	public Terceirizado(String nome, String cpf, Cargo cargo, BigDecimal salario, String empresa) {
+		this.dadosPessoais = new DadosPessoais(nome, cpf, cargo, salario);
+		this.empresa = empresa;
+	}
 	
 	public String getEmpresa() {
 		return empresa;
@@ -14,8 +20,20 @@ public class Terceirizado extends Funcionario{
 		this.empresa = empresa;
 	}
 
-	public Terceirizado(String nome, String cpf, Cargo cargo, BigDecimal salario) {
-		super(nome, cpf, cargo, salario);
+	public String getNome() {
+		return dadosPessoais.getNome();
+	}
+
+	public String getCpf() {
+		return dadosPessoais.getCpf();
+	}
+
+	public Cargo getCargo() {
+		return dadosPessoais.getCargo();
+	}
+
+	public BigDecimal getSalario() {
+		return dadosPessoais.getSalario();
 	}
 
 }
